51c2c7f227bb6c8872b26ce3e4f07f8bdb7b52fd
Matheus Nivaldo <matheus.nivaldo1997@gmail.com>
2020-03-04 22:26:23 -0300

    Aula 1

---
+++ Aulas/basic.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# nome = "Matheus Nivaldo"
+# idade = 22
+# altura = 1.76
+# brasileiro = True
+
+# # Primeira opção
+# print("Oi meu nome é", nome)
+# # Segunda opção
+# print("Oi meu nome é {}".format(nome))
+# # Terceira opção
+# print(f'Oi meu nome é: {nome}')
+
+# # Vendo tipos de dados
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+# Ex1
+# nome = "Matheus"
+# sobrenome = "Nivaldo"
+# idade = 22
+# print(f"Olá meu nome é {nome} {sobrenome} e tenho {idade} anos")
+# print(nome,idade,sobrenome)
+
+# # Entrada basica de dados
+# nome = input('Digite seu nome: ')
+# print(f'O nome do usuario e {nome}')
+
+# num1 = int(input('Digite o primeiro numero: '))
+# num2 = int(input('Digite o segundo numero: '))
+# soma = num1+num2
+# print(str(soma))
+
+texto = 'AAAAAAAAAAAAAAAAAAAA\
+        sssssssssssssssssssssss\
+        sssssssssssssssssssss'
+
+print(texto.upper())
+print(texto.lower())
+print(texto.capitalize())
+print(texto.split(' '))
---
+++ Exercicios/basic_ex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 1. Crie um script com 2 variaveis
+# nome e sobrenome
+# e imprima na tela
+# a mensagem 'Olá meu nome é nome sobrenome
+
+nome = "Matheus"
+sobrenome = "Nivaldo"
+idade = 22
+print(f"Olá meu nome é {nome} {sobrenome} e tenho {idade} anos")
+print(nome,idade,sobrenome)
+
+# msg = input("Digite sua mensagem: ")
+# print(msg)
